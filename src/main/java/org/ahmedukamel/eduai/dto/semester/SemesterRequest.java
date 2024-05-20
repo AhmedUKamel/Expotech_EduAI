ed940b552ef9a83e5ec35b765fba5446a82e3daa
Ahmed K. <ahmedukamel@outlook.com>
2024-05-20 23:04:34 +0300

    fix semester create/update validation

--- src/main/java/org/ahmedukamel/eduai/dto/semester/SemesterRequest.java
+++ src/main/java/org/ahmedukamel/eduai/dto/semester/SemesterRequest.java
@@ -1,12 +1,18 @@
 package org.ahmedukamel.eduai.dto.semester;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record SemesterRequest(
+        @NotBlank
         String name,
 
+        @NotNull
         LocalDate start,
 
+        @NotNull
         LocalDate end
 ) {
 }
