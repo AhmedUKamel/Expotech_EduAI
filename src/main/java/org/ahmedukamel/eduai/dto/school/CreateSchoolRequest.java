75172ed853c93e9aad9578b0c1924c59b07a9614
Ahmed K. <ahmedukamel@outlook.com>
2024-05-20 22:41:31 +0300

    fix school create/update validation

--- src/main/java/org/ahmedukamel/eduai/dto/school/CreateSchoolRequest.java
+++ src/main/java/org/ahmedukamel/eduai/dto/school/CreateSchoolRequest.java
@@ -1,20 +1,28 @@
 package org.ahmedukamel.eduai.dto.school;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.ahmedukamel.eduai.model.enumeration.Language;
 
 import java.time.LocalDate;
 
 public record CreateSchoolRequest(
+        @NotBlank
         String name,
 
+        @NotNull
         LocalDate established,
 
+        @NotBlank
         String about,
 
+        @NotNull
         Language language,
 
+        @NotBlank
         String code,
 
+        @NotBlank
         String theme
 ) {
 }
--- src/main/java/org/ahmedukamel/eduai/dto/school/UpdateSchoolRequest.java
+++ src/main/java/org/ahmedukamel/eduai/dto/school/UpdateSchoolRequest.java
@@ -1,14 +1,20 @@
 package org.ahmedukamel.eduai.dto.school;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.ahmedukamel.eduai.model.enumeration.Language;
 
 public record UpdateSchoolRequest(
+        @NotBlank
         String name,
 
+        @NotBlank
         String about,
 
+        @NotNull
         Language language,
 
+        @NotNull
         String theme
 ) {
 }
