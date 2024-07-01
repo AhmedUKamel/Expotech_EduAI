b8704d36646dc6a62c2bbc20f93c1d9f5a186b1a
Ahmed K. <ahmedukamel@outlook.com>
2024-07-01 15:25:42 +0300

    update parent cont

---
+++ src/main/java/org/ahmedukamel/eduai/dto/parent/AddParentRequest.java
@@ -0,0 +1,93 @@
+package org.ahmedukamel.eduai.dto.parent;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.ahmedukamel.eduai.annotation.ExistRegion;
+import org.ahmedukamel.eduai.annotation.UniquePhone;
+import org.ahmedukamel.eduai.annotation.UserUnique;
+import org.ahmedukamel.eduai.annotation.enumeration.UniquePhoneConstraint;
+import org.ahmedukamel.eduai.annotation.enumeration.UserUniqueConstraint;
+import org.ahmedukamel.eduai.constant.RegexConstants;
+import org.ahmedukamel.eduai.model.enumeration.Gender;
+import org.ahmedukamel.eduai.model.enumeration.Nationality;
+import org.ahmedukamel.eduai.model.enumeration.Religion;
+
+import java.time.LocalDate;
+
+public record AddParentRequest(
+        @NotBlank
+        @UserUnique(constraint = UserUniqueConstraint.USERNAME)
+        String username,
+
+        @Email
+        @NotBlank
+        @UserUnique(constraint = UserUniqueConstraint.EMAIL)
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = RegexConstants.PASSWORD)
+        String password,
+
+        @NotBlank
+        @UserUnique(constraint = UserUniqueConstraint.NID)
+        String nid,
+
+        @NotNull
+        Religion religion,
+
+        @NotNull
+        Gender gender,
+
+        @NotNull
+        Nationality nationality,
+
+        @NotNull
+        @ExistRegion
+        Integer regionId,
+
+        @NotNull
+        LocalDate birthDate,
+
+        @NotBlank
+        @UniquePhone(entity = UniquePhoneConstraint.PARENT)
+        String number,
+
+        @NotBlank
+        String firstName_en,
+
+        @NotBlank
+        String firstName_ar,
+
+        @NotBlank
+        String firstName_fr,
+
+        @NotBlank
+        String lastName_en,
+
+        @NotBlank
+        String lastName_ar,
+
+        @NotBlank
+        String lastName_fr,
+
+        @NotBlank
+        String about_en,
+
+        @NotBlank
+        String about_ar,
+
+        @NotBlank
+        String about_fr,
+
+        @NotBlank
+        String occupation_en,
+
+        @NotBlank
+        String occupation_ar,
+
+        @NotBlank
+        String occupation_fr
+) implements IParentRegistrationRequest {
+}
---
+++ src/main/java/org/ahmedukamel/eduai/dto/parent/IParentRegistrationRequest.java
@@ -0,0 +1,13 @@
+package org.ahmedukamel.eduai.dto.parent;
+
+import org.ahmedukamel.eduai.dto.auth.IUserRegistrationRequest;
+
+public interface IParentRegistrationRequest extends IUserRegistrationRequest {
+    String number();
+
+    String occupation_en();
+
+    String occupation_ar();
+
+    String occupation_fr();
+}
